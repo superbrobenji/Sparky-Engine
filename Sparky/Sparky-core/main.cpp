77a7e8983cd5e5a24411c44ac29f60a225e92272
superbrobenji <49689582+superbrobenji@users.noreply.github.com>
2019-04-17 11:20:45 +0200

    Project files added

---
+++ Sparky/Sparky-core/main.cpp
@@ -0,0 +1,26 @@
+#include "src/graphics/window.h"
+
+int main() {
+	using namespace sparky;
+	using namespace graphics;
+
+	Window window("Sparky!", 960, 540);
+
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+
+	while (!window.closed()) {
+
+		std::cout << window.getWidth() << ", " << window.getHeight() << std::endl;
+		window.clear();
+		glBegin(GL_QUADS);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(-0.5f, 0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+		window.update();
+	}
+
+	return 0;
+}
